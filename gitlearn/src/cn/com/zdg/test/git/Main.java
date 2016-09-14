40f6514b102705dee787f0e05324f285c87e432f
jblzdg <jblzdg@163.com>
2016-09-14 11:38:00 +0800

    init

---
+++ gitlearn/src/cn/com/zdg/test/git/Main.java
@@ -0,0 +1,30 @@
+/** 
+ * @Title: Main.java 
+ * @Package cn.com.zdg.test.git 
+ * @Description: TODO
+ * @author: Bruce Zou (jblzdg@163.com)
+ * @date 2016年9月14日 上午11:36:53 
+ * Copyright © 2016 深圳中电国际信息科技有限公司版权所有
+ */  
+package cn.com.zdg.test.git;
+
+/** 
+ * @Description: TODO
+ * @author Bruce Zou(jblzdg@163.com)
+ * @date 2016年9月14日 上午11:36:53  
+ */
+public class Main {
+
+	/** 
+	 * @Title: main 
+	 * @Description: TODO(这里用一句话描述这个方法的作用) 
+	 * @param @param args    设定文件 
+	 * @return void    返回类型 
+	 * @throws 
+	 */
+	public static void main(String[] args) {
+		System.out.println("init construct!!!");
+
+	}
+
+}
